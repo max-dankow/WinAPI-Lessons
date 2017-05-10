f7d0b30e0e262106ec9acc94157975c0b54d9713
Maxim Dankovtsev <max.dankow@gmail.com>
2017-05-10 21:50:13 +0300

    [Editor] Создал диалоговое окно настроек

--- TextEditor/TextEditor/resource.h
+++ TextEditor/TextEditor/resource.h
@@ -6,7 +6,7 @@
 #define IDD_TEXTEDITOR_DIALOG           102
 #define IDS_APP_TITLE                   103
 #define IDM_ABOUT                       104
-#define IDS_ON_QUIT_MESSAGE             104
+#define ID_VIEW_SETTINGS                32771
 #define IDM_EXIT                        105
 #define IDC_TEXTEDITOR                  109
 #define IDR_MAINFRAME                   128
@@ -17,9 +17,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        129
-#define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
