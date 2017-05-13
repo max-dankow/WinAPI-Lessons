1761ffab2a5642db391569614f81d76a1ac4235e
Данковцев Максим <max.dankow@gmail.com>
2017-05-13 17:32:24 +0300

    Восстановил greyscale

--- Binarization/greyscale.h
+++ Binarization/greyscale.h
@@ -1,4 +1,24 @@
 #ifndef GREYSCALE_H
 #define GREYSCALE_H
+#include "iimagefilter.h"
+
+class GreyScale : public IImageFilter {
+public:
+    QImage apply(const QImage &source) {
+        QImage dest(source);
+        for (int i = 0; i < dest.height(); ++i) {
+            QRgb* line = reinterpret_cast<QRgb*>(dest.scanLine(i));
+            for (int j = 0; j < dest.width(); ++j) {
+                auto grey =
+                        0.21 * qRed(line[j]) +
+                        0.72 * qGreen(line[j]) +
+                        0.07 * qBlue(line[j]);
+
+                line[j] = qRgb(grey, grey, grey);
+            }
+        }
+        return dest;
+    }
+};
 
 #endif // GREYSCALE_H
