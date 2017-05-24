8c468439c9f367a88a400a4430fd3cd59cc9d63d
Maxim Dankovtsev <max.dankow@gmail.com>
2017-05-25 02:58:41 +0300

    Вроде как морфология

--- VIdeoCapture/VIdeoCapture/MotionDetector.cpp
+++ VIdeoCapture/VIdeoCapture/MotionDetector.cpp
@@ -1,4 +1,5 @@
 #include "MotionDetector.h"
+#include <array>
 
 std::vector<std::vector<int>> CMotionDetector::Detect(const CBitmap & previous, const CBitmap & current) {
     std::vector< std::vector< int > > moveMask(current.GetHeight(), std::vector< int >(current.GetWidth(), 0));
@@ -23,29 +24,78 @@ std::vector<std::vector<int>> CMotionDetector::Detect(const CBitmap & previous, 
     return moveMaskWithoutNoise;
 }
 
-void CMotionDetector::SimpleNoiseFiter(const std::vector<std::vector<int>>& moveMask, std::vector<std::vector<int>>& dest, int height, int width) {
+void erosion(const std::vector<std::vector<int>>& source,
+    const std::vector<std::vector<int>>& mask,
+    std::vector<std::vector<int>>& dest,
+    int height, int width, int maskH, int maskW) 
+{
+    dest.assign(height, std::vector<int>(width, 0));
+
     for (int i = 0; i < width; i++) {
         for (int j = 0; j < height; j++) {
-            float sum = 0.f;
-            int count = 0;
-            for (int di = -2; di <= 2; di++) {
-                for (int dj = -2; dj <= 2; dj++) {
+            bool logicalAnd = true;
+            for (int di = -maskW; di <= maskW; di++) {
+                for (int dj = -maskH; dj <= maskH; dj++) {
                     int x = i + di;
                     int y = j + dj;
                     if (x >= 0 && x < width && y >= 0 && y < height) {
-                        sum += moveMask[j][i];
-                        count += 1;
+                        if (mask[dj + maskH][di + maskW] == 1) {
+                            if (source[y][x] < 0.5) {
+                                logicalAnd = false;
+                                break;
+                            }
+                        }
                     }
                 }
+                if (logicalAnd == false) {
+                    break;
+                }
             }
-            if (2 * sum < count) {
-                dest[j][i] = 1;
+            if (logicalAnd) {
+                dest[j][i] = 0;
             }
             else {
-                dest[j][i] = 0;
+                dest[j][i] = 1;
             }
         }
     }
+}
+
+void dilatation(const std::vector<std::vector<int>>& source,
+    const std::vector<std::vector<int>>& mask,
+    std::vector<std::vector<int>>& dest,
+    int height, int width, int maskH, int maskW)
+{
+    dest.assign(height, std::vector<int>(width, 0));
+
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            if (source[j][i] > 0.5) {
+                for (int di = -maskW; di <= maskW; di++) {
+                    for (int dj = -maskH; dj <= maskH; dj++) {
+                        int x = i + di;
+                        int y = j + dj;
+                        if (x >= 0 && x < width && y >= 0 && y < height) {
+                            if (mask[dj + maskH][di + maskW] == 1) {
+                                dest[y][x] = 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+void CMotionDetector::SimpleNoiseFiter(const std::vector<std::vector<int>>& moveMask, std::vector<std::vector<int>>& dest, int height, int width) {
+    const int maskH = 1, maskW = 1;
+    const std::vector<std::vector<int>> mask = { {0,1,0}, {1, 1, 1}, {0, 1, 0} };
+
+
+    std::vector<std::vector<int>> erosed(height, std::vector<int>(width, 0));
+    erosion(moveMask, mask, dest, height, width, maskH, maskW);
+    //dilatation(erosed, mask, dest, height, width, maskH, maskW);
 }
 
 void CMotionDetector::SaveMoveMaskToBitmap(const std::vector<std::vector<int>>& moveMask, CBitmap & dest) {
