76a4663ee0862c1406e893edbbdbe417dd489db8
Maxim Dankovtsev <max.dankow@gmail.com>
2017-05-24 17:11:47 +0300

    Refactor main cpp file

--- VIdeoCapture/VIdeoCapture/VIdeoCapture.cpp
+++ VIdeoCapture/VIdeoCapture/VIdeoCapture.cpp
@@ -1,10 +1,5 @@
 ﻿#include <windows.h>
-
-#include <Dshow.h>
-#include <objbase.h>
-
 #include <string>
-#include <exception>
 
 #include "Resource.h"
 #include "Utils.h"
@@ -13,52 +8,38 @@
 #define MAX_LOADSTRING 100
 
 // Глобальные переменные:
-HINSTANCE hInst;                                // текущий экземпляр
 WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
-WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+                     _In_ LPWSTR lpCmdLine,
+                     _In_ int nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
     // Инициализация глобальных строк
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_VIDEOCAPTURE, szWindowClass, MAX_LOADSTRING);
-    //MyRegisterClass(hInstance);
     CVideoCaptureWindow videoWindow;
 
-    // Выполнить инициализацию приложения:
-    //auto mainWindow = InitInstance(hInstance, nCmdShow, &videoWindow);
-    //if (!mainWindow) {
-    //    return 1;
-    //}
     CoInitialize(NULL);
     try {
         CVideoCaptureWindow::RegisterClass();
         videoWindow.Create(NULL);
         videoWindow.Show(nCmdShow);
         videoWindow.StartPreview();
-    }
-    catch (std::wstring errorMessage) {
-        ShowError(errorMessage);
-    }
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_VIDEOCAPTURE));
-    MSG msg;
-
-    // Цикл основного сообщения:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        MSG msg;
+        // Цикл основного сообщения:
+        while (GetMessage(&msg, nullptr, 0, 0))
         {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
+        return (int)msg.wParam;
+    } catch (std::wstring errorMessage) {
+        ShowError(errorMessage);
+        return 1;
     }
 
-    return (int) msg.wParam;
 }
